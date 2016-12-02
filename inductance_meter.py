bd15f4fa9515ae5496329204765e474dc3e732a2
Nick Trossa <nicolas.torasso@gmail.com>
2016-12-02 04:39:50 -0300

    Agrego esquema de induct-meter

---
+++ inductance_meter.py
@@ -0,0 +1,28 @@
+import numpy as np
+import serial
+
+"""
+Cosas de comenzar comunicacion con osc y gf
+"""
+
+"""
+Funciones para adquirir datos del osciloscopio
+Seteo de escala automatico segun frecuencia fijada
+"""
+
+
+"""
+Loop de barrido y adquisición --> f x1 e1 x2 e2 (frecuencia y amplitudes con incertezas)
+"""
+
+"""
+armo los input del ajuste
+g
+eg
+beta
+ebeta
+"""
+
+"""
+Hago el ajuste y calculo la inductancia con su error
+"""
